06788ac91b18b88d2d2f865eab05c83c5f69b8b9
crumblingstatue <radiantstatue@gmail.com>
2021-09-23 18:20:38 +0200

    Convert unwraps to propagation in main

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,14 @@ fn main() -> anyhow::Result<()> {
     if !atty::is(atty::Stream::Stdout) {
         return Ok(());
     }
-    let dir = env::current_dir().unwrap();
+    let dir = env::current_dir()?;
     let mut db = LocalDb::load_from_fs().unwrap_or_else(|e| {
         eprintln!("Error loading db: {}, creating new default db.", e);
         LocalDb::default()
     });
-    db.update_from_folder(&dir).unwrap();
+    db.update_from_folder(&dir)?;
     let mut no_save = false;
-    gui::run(&mut db, &mut no_save).unwrap();
+    gui::run(&mut db, &mut no_save)?;
     if !no_save {
         db.save_to_fs()?;
     }
