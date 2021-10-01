59a029a2ee722744eb46809ec886a13ceb38ad52
crumblingstatue <radiantstatue@gmail.com>
2021-10-01 19:39:37 +0200

    Switch from bincode to messagepack as serialization format

--- src/serialization.rs
+++ src/serialization.rs
@@ -4,15 +4,11 @@ use std::{
     path::Path,
 };
 
-use bincode::{DefaultOptions, Options};
+use rmp_serde::{encode::write_named, from_read};
 use serde::{Deserialize, Serialize};
 
-fn bincode_options() -> impl Options {
-    DefaultOptions::new().with_limit(2147483648)
-}
-
 pub fn read<T: for<'de> Deserialize<'de>>(source: impl Read) -> anyhow::Result<T> {
-    Ok(bincode_options().deserialize_from(source)?)
+    Ok(from_read(source)?)
 }
 
 pub fn read_from_file<T: for<'de> Deserialize<'de>>(path: impl AsRef<Path>) -> anyhow::Result<T> {
@@ -23,6 +19,6 @@ pub fn write_to_file<T: Serialize>(obj: &T, path: impl AsRef<Path>) -> anyhow::R
     write(obj, File::create(path)?)
 }
 
-pub fn write<T: Serialize>(obj: &T, sink: impl Write) -> anyhow::Result<()> {
-    Ok(bincode_options().serialize_into(sink, obj)?)
+pub fn write<T: Serialize>(obj: &T, mut sink: impl Write) -> anyhow::Result<()> {
+    Ok(write_named(&mut sink, obj)?)
 }
