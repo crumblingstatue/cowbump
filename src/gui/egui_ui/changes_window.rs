92c2a6d8759bb339a67086e3c5e46b3077788a89
crumblingstatue <radiantstatue@gmail.com>
2021-10-16 11:54:02 +0200

    Improve changes window ui

--- src/gui/egui_ui/changes_window.rs
+++ src/gui/egui_ui/changes_window.rs
@@ -22,33 +22,52 @@ pub(super) fn do_frame(
     }
     let changes = &win.changes;
     Window::new("Changes to collection").show(egui_ctx, |ui| {
-        if !changes.add.is_empty() {
-            ui.heading("Added");
-            ScrollArea::vertical().show(ui, |ui| {
-                for add in &changes.add {
-                    let label =
-                        Label::new(add.to_string_lossy().as_ref()).text_color(Color32::GREEN);
-                    ui.add(label);
-                }
-            });
-        }
-        if !changes.remove.is_empty() {
-            ui.heading("Removed");
-            ScrollArea::vertical().show(ui, |ui| {
-                for rem in &changes.remove {
-                    let label = Label::new(rem.to_string_lossy().as_ref()).text_color(Color32::RED);
-                    ui.add(label);
-                }
-            });
-        }
-        if ui.button("Apply").clicked() {
-            app.apply_changes_to_active_collection(changes);
-            state.entries_view = EntriesView::from_collection(app.active_collection().unwrap().1);
-            win.open = false;
-        }
-        if ui.button("Ignore").clicked() {
-            win.open = false;
-        }
+        ui.horizontal(|ui| {
+            if !changes.add.is_empty() {
+                ui.vertical(|ui| {
+                    ui.set_width(300.);
+                    ui.set_height(600.);
+                    ui.heading("Added");
+                    ScrollArea::vertical()
+                        .id_source("scroll_add")
+                        .show(ui, |ui| {
+                            for add in &changes.add {
+                                let label = Label::new(add.to_string_lossy().as_ref())
+                                    .text_color(Color32::GREEN);
+                                ui.add(label);
+                            }
+                        });
+                });
+            }
+            if !changes.remove.is_empty() {
+                ui.vertical(|ui| {
+                    ui.set_height(600.);
+                    ui.set_width(300.);
+                    ui.heading("Removed");
+                    ScrollArea::vertical()
+                        .id_source("scroll_rm")
+                        .show(ui, |ui| {
+                            for rem in &changes.remove {
+                                let label = Label::new(rem.to_string_lossy().as_ref())
+                                    .text_color(Color32::RED);
+                                ui.add(label);
+                            }
+                        });
+                });
+            }
+        });
+        ui.separator();
+        ui.horizontal(|ui| {
+            if ui.button("Apply").clicked() {
+                app.apply_changes_to_active_collection(changes);
+                state.entries_view =
+                    EntriesView::from_collection(app.active_collection().unwrap().1);
+                win.open = false;
+            }
+            if ui.button("Ignore").clicked() {
+                win.open = false;
+            }
+        });
     });
 }
 impl ChangesWindow {
