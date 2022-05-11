6ce79b44e7759b2f96e93882b740a5e064ea2f0a
crumblingstatue <radiantstatue@gmail.com>
2022-05-11 21:31:44 +0200

    Use query_window in find_popup::do_frame

--- src/gui/egui_ui/find_popup.rs
+++ src/gui/egui_ui/find_popup.rs
@@ -1,5 +1,5 @@
 use egui_sfml::{
-    egui::{Align2, Color32, Context, TextEdit},
+    egui::{Color32, Context, TextEdit},
     sfml::graphics::{RenderTarget, RenderWindow},
 };
 
@@ -8,7 +8,7 @@ use crate::{
     gui::{search_goto_cursor, State},
 };
 
-use super::EguiState;
+use super::{query_popup::query_window, EguiState};
 
 pub(super) fn do_frame(
     state: &mut State,
@@ -18,39 +18,35 @@ pub(super) fn do_frame(
     win: &RenderWindow,
 ) {
     if egui_state.find_popup.on {
-        egui_sfml::egui::Window::new("Find")
-            .anchor(Align2::LEFT_TOP, [32.0, 32.0])
-            .title_bar(false)
-            .auto_sized()
-            .show(egui_ctx, |ui| {
-                ui.horizontal(|ui| {
-                    ui.label("find");
-                    let mut te = TextEdit::singleline(&mut egui_state.find_popup.string);
-                    if !state.search_success {
-                        te = te.text_color(Color32::RED);
+        query_window("Find").show(egui_ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("find");
+                let mut te = TextEdit::singleline(&mut egui_state.find_popup.string);
+                if !state.search_success {
+                    te = te.text_color(Color32::RED);
+                }
+                let re = ui.add(te);
+                match state
+                    .search_reqs
+                    .parse_and_resolve(&egui_state.find_popup.string, coll)
+                {
+                    Ok(()) => (),
+                    Err(e) => {
+                        ui.label(&format!("Error: {}", e));
                     }
-                    let re = ui.add(te);
-                    match state
-                        .search_reqs
-                        .parse_and_resolve(&egui_state.find_popup.string, coll)
-                    {
-                        Ok(()) => (),
-                        Err(e) => {
-                            ui.label(&format!("Error: {}", e));
-                        }
-                    }
-                    // Avoid a deadlock with this let binding.
-                    // Inlining it into the if condition causes a deadlock
-                    let lost_focus = re.lost_focus();
-                    if ui.input().key_pressed(egui_sfml::egui::Key::Enter) || lost_focus {
-                        egui_state.find_popup.on = false;
-                    }
-                    if re.changed() || ui.input().key_pressed(egui_sfml::egui::Key::Enter) {
-                        state.search_cursor = 0;
-                        search_goto_cursor(state, coll, win.size().y);
-                    }
-                    ui.memory().request_focus(re.id);
-                });
+                }
+                // Avoid a deadlock with this let binding.
+                // Inlining it into the if condition causes a deadlock
+                let lost_focus = re.lost_focus();
+                if ui.input().key_pressed(egui_sfml::egui::Key::Enter) || lost_focus {
+                    egui_state.find_popup.on = false;
+                }
+                if re.changed() || ui.input().key_pressed(egui_sfml::egui::Key::Enter) {
+                    state.search_cursor = 0;
+                    search_goto_cursor(state, coll, win.size().y);
+                }
+                ui.memory().request_focus(re.id);
             });
+        });
     }
 }
