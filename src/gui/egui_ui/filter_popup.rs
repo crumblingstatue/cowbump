872781ac58cb8cf7abaececf7d49571b09d5504d
crumblingstatue <radiantstatue@gmail.com>
2022-05-11 21:15:05 +0200

    Factor out query window creation code into query_popup module

--- src/gui/egui_ui/filter_popup.rs
+++ src/gui/egui_ui/filter_popup.rs
@@ -1,4 +1,4 @@
-use egui_sfml::egui::{Align2, Color32, Context, Key, Modifiers, TextEdit};
+use egui_sfml::egui::{Color32, Context, Key, Modifiers, TextEdit};
 
 use crate::{
     collection::Collection,
@@ -10,7 +10,7 @@ use crate::{
     },
 };
 
-use super::EguiState;
+use super::{query_popup::query_window, EguiState};
 
 /// Returns whether filter state changed
 pub(super) fn do_frame(
@@ -23,70 +23,66 @@ pub(super) fn do_frame(
     let mut text_changed = false;
     let mut success = false;
     if popup.on {
-        egui_sfml::egui::Window::new("Filter")
-            .anchor(Align2::LEFT_TOP, [32.0, 32.0])
-            .title_bar(false)
-            .auto_sized()
-            .show(egui_ctx, |ui| {
-                ui.horizontal(|ui| {
-                    ui.label("filter");
-                    let count = coll.filter(&state.filter).count();
-                    let te_id = ui.make_persistent_id("text_edit_tag_popup");
-                    let up_pressed = ui
-                        .input_mut()
-                        .consume_key(Modifiers::default(), Key::ArrowUp);
-                    let down_pressed = ui
-                        .input_mut()
-                        .consume_key(Modifiers::default(), Key::ArrowDown);
-                    let mut te = TextEdit::singleline(&mut popup.string)
-                        .lock_focus(true)
-                        .id(te_id);
-                    if count == 0 {
-                        te = te.text_color(Color32::RED);
-                    }
-                    if popup.ac_state.applied {
-                        text_edit_cursor_set_to_end(ui, te_id);
-                    }
-                    let re = ui.add(te);
-                    if tag_autocomplete_popup(
-                        &mut popup.string,
-                        &mut popup.ac_state,
-                        coll,
-                        ui,
-                        &re,
-                        up_pressed,
-                        down_pressed,
-                    ) {
-                        state.wipe_search();
-                        text_changed = true;
-                    }
-                    ui.label(&format!("{} results", count));
-                    popup.string.make_ascii_lowercase();
-                    let enter_pressed = egui_ctx.input().key_pressed(Key::Enter);
-                    if enter_pressed || egui_ctx.input().key_pressed(Key::Escape) {
-                        popup.on = false;
-                    }
-                    if re.changed() || text_changed || enter_pressed {
-                        popup.err_string.clear();
-                        match state.filter.parse_and_resolve(&popup.string, coll) {
-                            Ok(()) => {
-                                success = true;
-                            }
-                            Err(e) => {
-                                popup.err_string = format!("Error: {}", e);
-                                success = false;
-                            }
-                        }
-                        popup.ac_state.input_changed = true;
-                        state.wipe_search();
-                        text_changed = true;
-                    }
-                    ui.memory().request_focus(re.id);
-                });
-                if !popup.err_string.is_empty() {
-                    ui.label(&popup.err_string);
+        query_window("Filter").show(egui_ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("filter");
+                let count = coll.filter(&state.filter).count();
+                let te_id = ui.make_persistent_id("text_edit_tag_popup");
+                let up_pressed = ui
+                    .input_mut()
+                    .consume_key(Modifiers::default(), Key::ArrowUp);
+                let down_pressed = ui
+                    .input_mut()
+                    .consume_key(Modifiers::default(), Key::ArrowDown);
+                let mut te = TextEdit::singleline(&mut popup.string)
+                    .lock_focus(true)
+                    .id(te_id);
+                if count == 0 {
+                    te = te.text_color(Color32::RED);
                 }
+                if popup.ac_state.applied {
+                    text_edit_cursor_set_to_end(ui, te_id);
+                }
+                let re = ui.add(te);
+                if tag_autocomplete_popup(
+                    &mut popup.string,
+                    &mut popup.ac_state,
+                    coll,
+                    ui,
+                    &re,
+                    up_pressed,
+                    down_pressed,
+                ) {
+                    state.wipe_search();
+                    text_changed = true;
+                }
+                ui.label(&format!("{} results", count));
+                popup.string.make_ascii_lowercase();
+                let enter_pressed = egui_ctx.input().key_pressed(Key::Enter);
+                if enter_pressed || egui_ctx.input().key_pressed(Key::Escape) {
+                    popup.on = false;
+                }
+                if re.changed() || text_changed || enter_pressed {
+                    popup.err_string.clear();
+                    match state.filter.parse_and_resolve(&popup.string, coll) {
+                        Ok(()) => {
+                            success = true;
+                        }
+                        Err(e) => {
+                            popup.err_string = format!("Error: {}", e);
+                            success = false;
+                        }
+                    }
+                    popup.ac_state.input_changed = true;
+                    state.wipe_search();
+                    text_changed = true;
+                }
+                ui.memory().request_focus(re.id);
             });
+            if !popup.err_string.is_empty() {
+                ui.label(&popup.err_string);
+            }
+        });
     }
     text_changed && success
 }
--- src/gui/egui_ui/query_popup.rs
+++ src/gui/egui_ui/query_popup.rs
@@ -1,3 +1,5 @@
+use egui_sfml::egui::{Align2, Window};
+
 use super::tag_autocomplete::AcState;
 
 #[derive(Default)]
@@ -6,4 +8,12 @@ pub struct QueryPopup {
     pub string: String,
     pub err_string: String,
     pub ac_state: AcState,
+}
+
+/// Create an egui window in the corner for queries (filter/search)
+pub fn query_window(title: &str) -> Window {
+    Window::new(title)
+        .anchor(Align2::LEFT_TOP, [32.0, 32.0])
+        .title_bar(false)
+        .auto_sized()
 }
