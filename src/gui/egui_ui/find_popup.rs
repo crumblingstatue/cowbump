a47ec15b79ffe02e733b9fbf22dcf3422f060a50
crumblingstatue <radiantstatue@gmail.com>
2022-05-11 21:48:44 +0200

    Factor some common query popup ui code into QueryPopup::do_frame

--- src/gui/egui_ui/find_popup.rs
+++ src/gui/egui_ui/find_popup.rs
@@ -8,7 +8,7 @@ use crate::{
     gui::{search_goto_cursor, State},
 };
 
-use super::{query_popup::query_window, EguiState};
+use super::EguiState;
 
 pub(super) fn do_frame(
     state: &mut State,
@@ -17,36 +17,30 @@ pub(super) fn do_frame(
     coll: &mut Collection,
     win: &RenderWindow,
 ) {
-    if egui_state.find_popup.on {
-        query_window("Find").show(egui_ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("find");
-                let mut te = TextEdit::singleline(&mut egui_state.find_popup.string);
-                if !state.search_success {
-                    te = te.text_color(Color32::RED);
+    egui_state
+        .find_popup
+        .do_frame("find", egui_ctx, |popup, ui| {
+            let mut te = TextEdit::singleline(&mut popup.string);
+            if !state.search_success {
+                te = te.text_color(Color32::RED);
+            }
+            let re = ui.add(te);
+            match state.search_reqs.parse_and_resolve(&popup.string, coll) {
+                Ok(()) => (),
+                Err(e) => {
+                    ui.label(&format!("Error: {}", e));
                 }
-                let re = ui.add(te);
-                match state
-                    .search_reqs
-                    .parse_and_resolve(&egui_state.find_popup.string, coll)
-                {
-                    Ok(()) => (),
-                    Err(e) => {
-                        ui.label(&format!("Error: {}", e));
-                    }
-                }
-                // Avoid a deadlock with this let binding.
-                // Inlining it into the if condition causes a deadlock
-                let lost_focus = re.lost_focus();
-                if ui.input().key_pressed(egui_sfml::egui::Key::Enter) || lost_focus {
-                    egui_state.find_popup.on = false;
-                }
-                if re.changed() || ui.input().key_pressed(egui_sfml::egui::Key::Enter) {
-                    state.search_cursor = 0;
-                    search_goto_cursor(state, coll, win.size().y);
-                }
-                ui.memory().request_focus(re.id);
-            });
+            }
+            // Avoid a deadlock with this let binding.
+            // Inlining it into the if condition causes a deadlock
+            let lost_focus = re.lost_focus();
+            if ui.input().key_pressed(egui_sfml::egui::Key::Enter) || lost_focus {
+                popup.on = false;
+            }
+            if re.changed() || ui.input().key_pressed(egui_sfml::egui::Key::Enter) {
+                state.search_cursor = 0;
+                search_goto_cursor(state, coll, win.size().y);
+            }
+            ui.memory().request_focus(re.id);
         });
-    }
 }
--- src/gui/egui_ui/query_popup.rs
+++ src/gui/egui_ui/query_popup.rs
@@ -1,4 +1,4 @@
-use egui_sfml::egui::{Align2, Window};
+use egui_sfml::egui::{Align2, Context, Ui, Window};
 
 use super::tag_autocomplete::AcState;
 
@@ -11,9 +11,30 @@ pub struct QueryPopup {
 }
 
 /// Create an egui window in the corner for queries (filter/search)
-pub fn query_window(title: &str) -> Window {
+fn query_window(title: &str) -> Window {
     Window::new(title)
         .anchor(Align2::LEFT_TOP, [32.0, 32.0])
         .title_bar(false)
         .auto_sized()
+}
+
+impl QueryPopup {
+    pub fn do_frame(
+        &mut self,
+        title: &str,
+        egui_ctx: &Context,
+        mut inner_fn: impl FnMut(&mut Self, &mut Ui),
+    ) {
+        if self.on {
+            query_window(title).show(egui_ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label(title);
+                    inner_fn(self, ui);
+                });
+                if !self.err_string.is_empty() {
+                    ui.label(&self.err_string);
+                }
+            });
+        }
+    }
 }
