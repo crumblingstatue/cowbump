41a1952c9510d3982bd612073848d4cff067c9a6
crumblingstatue <radiantstatue@gmail.com>
2022-05-17 22:35:36 +0200

    Turn Application::save_collection into a method

--- src/application.rs
+++ src/application.rs
@@ -71,7 +71,7 @@ impl Application {
     }
     pub fn save_active_collection(&self) -> anyhow::Result<()> {
         match self.active_collection.as_ref() {
-            Some((id, coll)) => Self::save_collection(&self.database.data_dir, *id, coll),
+            Some((id, coll)) => self.save_collection(*id, coll),
             None => Ok(()),
         }
     }
@@ -80,17 +80,13 @@ impl Application {
         coll: Option<(collection::Id, Collection)>,
     ) -> anyhow::Result<()> {
         if let Some((id, coll)) = &self.active_collection {
-            Self::save_collection(&self.database.data_dir, *id, coll)?;
+            self.save_collection(*id, coll)?;
         }
         self.active_collection = coll;
         Ok(())
     }
-    fn save_collection(
-        data_dir: &Path,
-        id: collection::Id,
-        collection: &Collection,
-    ) -> anyhow::Result<()> {
-        let dir_name = collections_dir_name(data_dir);
+    fn save_collection(&self, id: collection::Id, collection: &Collection) -> anyhow::Result<()> {
+        let dir_name = collections_dir_name(&self.database.data_dir);
         std::fs::create_dir_all(&dir_name)?;
         serialization::write_to_file(collection, collection_filename(&dir_name, id))
     }
