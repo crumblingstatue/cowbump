dfd4bfc922c3b0d4c25016de70343aa08387ce1b
crumblingstatue <radiantstatue@gmail.com>
2024-01-19 18:59:05 +0100

    Add env_logger to access logs (for debugging)

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn try_main() -> anyhow::Result<()> {
 }
 
 fn main() {
+    env_logger::init();
     if let Err(e) = try_main() {
         gui::native_dialog::error("Fatal runtime error", e)
     }
