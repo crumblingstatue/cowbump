03e3960ea8e1382a1eddc2f332d93aed2928d6db
crumblingstatue <radiantstatue@gmail.com>
2024-10-03 17:35:44 +0200

    Collapse else-if

--- src/gui/thumbnails_view.rs
+++ src/gui/thumbnails_view.rs
@@ -378,10 +378,8 @@ pub(in crate::gui) fn handle_event(
             } else if code == Key::Enter {
                 if preferences.use_built_in_viewer {
                     builtin::on_enter_open(state, window);
-                } else {
-                    if let Err(e) = external::on_enter_open(state, coll, preferences) {
-                        egui_state.modal.err(format!("Failed to open file(s): {e}"));
-                    }
+                } else if let Err(e) = external::on_enter_open(state, coll, preferences) {
+                    egui_state.modal.err(format!("Failed to open file(s): {e}"));
                 }
             } else if code == Key::A && ctrl {
                 select_all(state, coll);
